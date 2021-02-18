6d210ba646e889d53b0fa7918a80ce86828f3491
annshiv <annamalaipalani11@gmail.com>
2021-02-18 18:49:08 +0530

    Data Types

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,6 +1,9 @@
 let word = 'annshiv';
 if (word === 'annshiv') alert("Welcome to my page");
 
+
+// values and variables 
+
 country = 'india';
 continent = 'asia';
 population = '7.5B';
@@ -8,3 +11,13 @@ population = '7.5B';
 console.log(country);
 console.log(continent);
 console.log(population);
+
+// Data types
+
+let isIsland = false;
+let language;
+
+console.log(typeof(isIsland));
+console.log(typeof(population));
+console.log(typeof(country));
+console.log(typeof(language));
