e3dd6e44f4cd29d1431e50459226e02c68ceb28b
annshiv <annamalaipalani11@gmail.com>
2021-02-18 18:42:31 +0530

    Values and Variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,2 +1,10 @@
 let word = 'annshiv';
 if (word === 'annshiv') alert("Welcome to my page");
+
+country = 'india';
+continent = 'asia';
+population = '7.5B';
+
+console.log(country);
+console.log(continent);
+console.log(population);
