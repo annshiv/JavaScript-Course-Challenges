19a750a023bd58cf7d1f00262e8fa72610bbcd23
annshiv <annamalaipalani11@gmail.com>
2021-03-02 23:09:08 +0530

    Type Conversion and Coercion

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -136,6 +136,14 @@ if (population1 > 33) {
     console.log(`${country}'s population is ${33 - population1} million below average`);
 }
 
+// Type Conversion and Coercion
+
+console.log('9' - '5');
+console.log('19' - '13' + '17');
+console.log('19' - '13' + 17);
+console.log('123' < 57);
+console.log(5 + 6 + '4' + 9 - 4 - 2);
+
 ////////////////////////////////////
 // Coding Challenge #4
 
