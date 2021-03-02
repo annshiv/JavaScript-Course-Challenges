e24fbdf349eb2c58cdb341bbcb0d64334efa0edb
annshiv <annamalaipalani11@gmail.com>
2021-03-02 23:40:02 +0530

    Switch statement

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -188,6 +188,30 @@ console.log(`You should live in ${country} :)`);
 console.log(`${country} does not meet your criteria :(`);
 }
 
+// The switch Statement
+
+let language1 = prompt("What is your language ?");
+switch (language1) {
+    case 'chinese':
+    case 'mandarin':
+        console.log('MOST number of native speakers!');
+        break
+    case 'spanish':
+        console.log('2nd place in number of native speakers');
+        break
+    case 'english':
+        console.log('3rd place');
+        break
+    case 'hindi':
+        console.log('number 4');
+        break
+    case 'arabic':
+        console.log('5th most spoken language');
+        break
+    default:
+        console.log('Great language too :D')
+}
+
 ////////////////////////////////////
 // Coding Challenge #4
 
