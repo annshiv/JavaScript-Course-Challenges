10a6f5ee5fabcbe6f4cd235bda016ef73c5a59e3
annshiv <annamalaipalani11@gmail.com>
2021-03-11 16:17:33 +0530

    Object method challenge

--- 02-Fundamentals-Part-2 /script.js
+++ 02-Fundamentals-Part-2 /script.js
@@ -111,3 +111,34 @@ console.log(tips)
 
 const total = [bills[0]+calcTip(bills[0]),bills[1]+calcTip(bills[1]),bills[2]+calcTip(bills[2])]
 console.log(total)
+
+// Object Methods
+
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  birthYeah: 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven'],
+  hasDriversLicense: true,
+
+  calcAge: function () {
+    this.age = 2037 - this.birthYeah;
+    return this.age;
+  },
+
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()}-year old ${jonas.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
+  }
+};
+
+console.log(jonas.calcAge());
+
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
+
+// Challenge
+// "Jonas is a 46-year old teacher, and he has a driver's license"
+
+console.log(jonas.getSummary());
